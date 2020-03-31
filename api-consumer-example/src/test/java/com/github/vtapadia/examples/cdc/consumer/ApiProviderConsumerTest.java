e21842e1e311f963c28b6fdd984d2f4f35efa64a
Varesh Tapadia <varesh.tapadia@rabobank.nl>
2020-03-31 15:16:56 +0200

    Added example value to test it properly.

--- api-consumer-example/src/test/java/com/github/vtapadia/examples/cdc/consumer/ApiProviderConsumerTest.java
+++ api-consumer-example/src/test/java/com/github/vtapadia/examples/cdc/consumer/ApiProviderConsumerTest.java
@@ -7,12 +7,14 @@ import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
 import au.com.dius.pact.core.model.RequestResponsePact;
 import au.com.dius.pact.core.model.annotations.Pact;
+import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.fluent.Request;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -31,7 +33,7 @@ public class ApiProviderConsumerTest {
                 .method("GET")
                 .willRespondWith()
                 .status(200)
-                .body(new PactDslJsonBody().stringType("name").integerType("id"))
+                .body(new PactDslJsonBody().stringType("name", "Varesh").integerType("id", 1))
                 .toPact();
     }
 
@@ -40,8 +42,8 @@ public class ApiProviderConsumerTest {
     void testArticles(MockServer mockServer) throws IOException {
         HttpResponse httpResponse = Request.Get(mockServer.getUrl() + "/api/employee/1").execute().returnResponse();
         assertThat(httpResponse.getStatusLine().getStatusCode(), is(equalTo(200)));
-//        assertThat(IOUtils.toString(httpResponse.getEntity().getContent()),
-//                is(equalTo("{\"articles\":[{\"variants\":{\"0032\":{\"description\":\"sample description\"}}}]}")));
+        assertThat(IOUtils.toString(httpResponse.getEntity().getContent(), Charset.forName("UTF-8")),
+                is(equalTo("{\"name\":\"Varesh\",\"id\":1}")));
     }
 
 }
